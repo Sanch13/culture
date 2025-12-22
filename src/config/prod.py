f6afaba632b9a684c1ac64f6a01d7072a47fda1e
sanch <sanch13sanch@gmail.com>
2025-12-22 15:54:19 +0300

    add custom page

---
+++ src/config/prod.py
@@ -0,0 +1,5 @@
+# from config.dev import *
+#
+# DEBUG = False
+# ALLOWED_HOSTS = settings.ALLOWED_HOSTS_FOR_DEPLOY
+# STATIC_ROOT = BASE_DIR / "static/"
--- src/config/urls.py
+++ src/config/urls.py
@@ -1,8 +1,22 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.shortcuts import render
 
 from django.conf import settings
 from django.conf.urls.static import static
+
+
+def custom_404(request, exception=None):
+    return render(request, "404.html", status=404)
+
+
+def custom_500(request):
+    return render(request, "500.html", status=500)
+
+
+handler404 = custom_404
+handler500 = custom_500
+
 
 urlpatterns = [
     path("auth/", include("users.urls")),
