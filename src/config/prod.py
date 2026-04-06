bb207106b659236b02de288ef40b35ea48ab34bf
sanch <sanch13sanch@gmail.com>
2026-04-06 12:49:42 +0300

    add fix https

--- src/config/prod.py
+++ src/config/prod.py
@@ -4,6 +4,13 @@ DEBUG = False
 ALLOWED_HOSTS = settings.ALLOWED_HOSTS_FOR_DEPLOY  # noqa: F405
 STATIC_ROOT = BASE_DIR / "static/"  # noqa: F405
 
+CSRF_TRUSTED_ORIGINS = [
+    settings.SITE_URL,  # noqa: F405
+]
+SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+
 if DEBUG:
     SITE_URL = "http://127.0.0.1:8000"
 else:
